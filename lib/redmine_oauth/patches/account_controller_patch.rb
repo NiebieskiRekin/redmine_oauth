5ea3dbddbe4883993a195d09c14e29924d766d09
Tomasz Pawłowski <tomasz.pawlowski@beone.pl>
2025-07-23 10:49:03 +0200

    fix path

--- lib/redmine_oauth/patches/account_controller_patch.rb
+++ lib/redmine_oauth/patches/account_controller_patch.rb
@@ -55,7 +55,7 @@ module RedmineOauth
           logout_user
           tenant_id = RedmineOauth.tenant_id
           client_id = RedmineOauth.client_id
-          redirect_to "#{site}#{RedmineOauth.oauth_path}/realms/#{tenant_id}/protocol/openid-connect/logout?id_token_hint=#{id_token}&client_id=#{client_id}&post_logout_redirect_uri=#{url}"
+          redirect_to "#{site+RedmineOauth.oauth_path}/realms/#{tenant_id}/protocol/openid-connect/logout?id_token_hint=#{id_token}&client_id=#{client_id}&post_logout_redirect_uri=#{url}"
         when 'Okta'
           logout_user
           redirect_to "#{site}/oauth2/v1/logout?id_token_hint=#{id_token}&post_logout_redirect_uri=#{url}"
